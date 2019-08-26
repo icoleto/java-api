4a5981cb99abc670b68db9e89adc76aa493aa87a
Ivan Coleto <ivancoleto@gmail.com>
2019-08-26 20:36:23 +0200

    add fibonacci function

---
+++ src/main/java/com/icoleto/javaapi/service/Functions.java
@@ -0,0 +1,11 @@
+package com.icoleto.javaapi.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class Functions {
+
+  public Long fibonacci(Long num) {
+    return num == 1 ? 1 : fibonacci(num - 1) + fibonacci(num - 2);
+  }
+}
